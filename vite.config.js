98fa404213a08c76912fd94b2c3a840795bab659
mhareeham111 <mariam.ibrahim0526@gmaill.com>
2025-07-28 14:22:58 +0100

    new push

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 import { fileURLToPath } from 'url';
-
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
